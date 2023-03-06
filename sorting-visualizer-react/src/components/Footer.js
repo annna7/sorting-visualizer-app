c6587a0436f3a2fde2df1f0a76cee0ce40291948
Anna Pecheanu <annapecheanu1@gmail.com>
2023-03-06 11:20:18 +0200

    Modified css files to make the web application aesthetically pleasing

--- sorting-visualizer-react/src/components/Footer.js
+++ sorting-visualizer-react/src/components/Footer.js
@@ -7,27 +7,29 @@ const Footer = ({arrayLength, sortingSpeed, isSorting, handleGen, changeLength, 
         <>
             <div className="footer">
                 <div className="footer-flex">
-                    <div className="gen-array">
-                        <button disabled={isSorting} onClick={handleGen}> Generate New Array </button>
+                    <div className="footer-flex-item">
+                        <div className="gen-array">
+                            <button className="big-button" disabled={isSorting} onClick={handleGen}> Generate New Array </button>
+                        </div>
+                        <div className="set-length">
+                            <p> Set Length </p>
+                            <input readOnly={isSorting} disabled={isSorting} type="range" id="length-slider" className="slider" min="50" max="200" value={arrayLength} onChange={(e) => changeLength(e.target.value)}></input>
+                        </div>
+                        <div className="set-speed">
+                            <p> Set Speed </p>
+                            <input readOnly={isSorting} disabled={isSorting} type="range" id="speed-slider" className="slider" min="1" max="3" step="1" value={sortingSpeed} onChange={(e) => changeSpeed(e.target.value)}></input>
+                        </div>
                     </div>
-                    <div className="set-length">
-                        <p> Set Length </p>
-                        <input readOnly={isSorting} disabled={isSorting} type="range" id="length-slider" className="slider" min="50" max="200" value={arrayLength} onChange={(e) => changeLength(e.target.value)}></input>
+                    <div className="footer-flex-item">
+                        <div className="select-algo">
+                            <p> Select algorithm</p>
+                            <button disabled={isSorting} onClick={() => changeAlgo("bubble")}>Bubble Sort</button>
+                            <button disabled={isSorting} onClick={() => changeAlgo("select")}>Select Sort</button>
+                            <button disabled={isSorting} onClick={() => changeAlgo("merge")}>Merge Sort</button>
+                            <button disabled={isSorting} onClick={() => changeAlgo("heap")}>Heap Sort</button>
+                        </div>
+                        <button className="big-button" disabled={isSorting} onClick={startSorting}>Start!</button>
                     </div>
-                    <div className="set-speed">
-                        <p> Set Speed </p>
-                        <input readOnly={isSorting} disabled={isSorting} type="range" id="speed-slider" className="slider" min="1" max="3" step="1" value={sortingSpeed} onChange={(e) => changeSpeed(e.target.value)}></input>
-                    </div>
-                    <div className="select-algo">
-                        <p> Select algorithm</p>
-                        <select disabled={isSorting} onChange={(e) => changeAlgo(e.target.value)}>
-                            <option value="bubble">Bubble Sort</option>
-                            <option value="select">Select Sort</option>
-                            <option value="merge">Merge Sort</option>
-                            <option value="heap">Heap Sort</option>
-                        </select>
-                    </div>
-                    <button disabled={isSorting} className="start-button" onClick={startSorting}>Start!</button>
                 </div>
             </div>
         </>
--- sorting-visualizer-react/src/components/SortingVisualizer.js
+++ sorting-visualizer-react/src/components/SortingVisualizer.js
@@ -1,15 +1,13 @@
-import React, {useState} from 'react';
+import React from 'react';
 import Bar from './Bar';
 import './SortingVisualizer.css'
-import uniqid from 'uniqid';
-
 
 const SortingVisualizer = ({arr}) => {
     const toColor = (code) => {
         switch (code) {
-            case 0: return "darkgreen";
-            case 1: return "red";
-            case 2: return "blue";
+            case 0: return "#9797ff";
+            case 1: return "#69417a";
+            case 2: return "#091459";
         }
     }
     const sortingBars = arr.map((value, index) => (
